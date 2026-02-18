107f6cb25c63a524fc0597c0b9db6a70ac201f9c
𝒛𝒂𝒓𝒂 <zaradinaadjepong@gmail.com>
2026-02-18 21:38:39 +0000

    Delete score.js
    Signed-off-by: 𝒛𝒂𝒓𝒂 <zaradinaadjepong@gmail.com>

--- score.js
+++
@@ -1,7 +0,0 @@
-let playerScore=500;
-let level =5;
-let bonusPoints=true;
-if (bonusPoints && level >= 5) {
-    playerScore += 100; 
-    console.log(`Congratulations! You've earned bonus points. Your new score is ${playerScore}.`);
-}

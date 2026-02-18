8d6d9bc7243c1e2dcb577138979d025228bc3361
𝒛𝒂𝒓𝒂 <zaradinaadjepong@gmail.com>
2026-02-18 21:39:16 +0000

    Delete index.js
    Signed-off-by: 𝒛𝒂𝒓𝒂 <zaradinaadjepong@gmail.com>

--- index.js
+++
@@ -1,7 +0,0 @@
-const FreezingPoint = 0;
-let tempCelsius = 20;
-let tempFahrenheit = (tempCelsius * 9/5) + 32;
-console.log(`${tempCelsius}°C is equal to ${tempFahrenheit}°F`);
-if (tempCelsius <= FreezingPoint) {
-    console.log("Warning: Temperature is at or below freezing point!");
-}
